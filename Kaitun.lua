f07289ee71e652334c561c3cb98c7a33f95b4e2c
Tgodyeuem <congtu.sv.4.1@gmail.com>
2024-08-21 02:58:24 +0700

    Update Kaitun.lua

--- Kaitun.lua
+++ Kaitun.lua
@@ -2,9 +2,11 @@ local CommitID = '9f1d465ee1e0b266002231869b06ccd227e975b3'
 function LoadSI(ScriptName,commit)
     if not commit then commit = CommitID end 
     if not commit then commit = 'main' end
-    loadstring(game:HttpGet("https://github.com/Tgodyeuem/kaituntest/blob/main/Kaitun.lua"))()
+    local scripturl = 'https://raw.githubusercontent.com/Tgodyeuem/kaituntest/'..commit..'/'..ScriptName
+    loadstring(game:HttpGet(scripturl))()
 end
-task.spawn(function()
+LoadSI('NewFastAttackLoader.lua')
+LoadSI('StatusGuiV2.lua')
 LoadSI('EnviromentFunctionBF.lua')
 LoadSI('ConfigLoader.lua')
 LoadSI('AutoL.lua')  
