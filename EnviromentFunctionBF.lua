75629a95444bd40246f3b733f5bc7ff13841d591
Tgodyeuem <congtu.sv.4.1@gmail.com>
2024-08-20 22:44:36 +0700

    Update EnviromentFunctionBF.lua

--- EnviromentFunctionBF.lua
+++ EnviromentFunctionBF.lua
@@ -61,8 +61,8 @@ if game.Workspace:FindFirstChild("MobSpawns") then
     end
 end
 _G.SavedConfig = type(_G.SavedConfig) == 'table' and _G.SavedConfig or {}
---loadstring(game:HttpGet('https://raw.githubusercontent.com/memaybeohub/NewPage/main/FastAttackLoading.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/memaybeohub/NewPage/main/HopLoader.lua'))()
+--loadstring(game:HttpGet('https://raw.githubusercontent.com/Tgodyeuem/kaituntest/main/FastAttackLoading.lua.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Tgodyeuem/kaituntest/main/HopLoader.lua'))()
 function GetDistance(target1, taget2)
     if not taget2 then
         pcall(function()
